5086bb4156b841591b040b8662b971d2a5dbacbf
ninaglss15 <10526@holbertonstudents.com>
2025-02-14 12:38:45 +0000

    6-size.c

---
+++ hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prinf see all types octes (char etc)
+ * return 0
+ */
+int main(void)
+{
+	printf("%d\n", (int) sizeof(char));
+	printf("%d\n", (int) sizeof(int));
+	printf("%d\n", (int) sizeof(long));
+	printf("%d\n", (int) sizeof(float));
+	printf("%d\n", (int) sizeof(double));
+	printf("%d\n", (int) sizeof(void));
+
+	return (0);
+
+
