c3e84b4c666538d16f1f751b221c10a7c36c67e1
ninaglss15 <10526@holbertonstudents.com>
2025-02-14 09:48:56 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
-int main () {
-	puts ("Programming is like building a multilingual puzzle\n")
-		return 0
+int main (void) {
+	puts("Programming is like building a multilingual puzzle");
+		return 0;
+}
+
+
+
+
 
