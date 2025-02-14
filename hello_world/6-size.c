80372822d880f2c19197e1b90cb29fd44ed6198d
ninaglss15 <10526@holbertonstudents.com>
2025-02-14 14:26:14 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * main - prinf see all types octes (char etc)
- * return 
+ * main - printf
+ * return0
  */
 int main(void)
 {
-	char a;
-	int b;
+	int p;
+	char u;
 	long int c;
-	long long int d;
-	float f;
+	float e;
+	long long s;
 
-	printf("size of a char: %d byte(s)\n", (int) sizeof(a));
-	printf("size of a int: %d byte(s)\n", (int) sizeof(b));
-	printf("size of a long int: %d byte(s)\n", (int) sizeof(c));
-	printf("size of a lolng lolng int: %d byte(s)\n", (int) sizeof(d));
-	printf("size of a float: %d byte(s)\n", (int) sizeof(f));
+	printf("Size of a int: %d byte(s)\n", (int) sizeof(p));
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(u));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(c));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(e));
+	printf("Size of a long long: %d byte(s)\n", (int) sizeof(s));
 
 	return (0);
 }
