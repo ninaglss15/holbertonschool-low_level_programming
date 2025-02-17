328a934c9465441fb9d034f1d878be9b035cef08
ninaglss15 <10526@holbertonstudents.com>
2025-02-17 10:29:19 +0000

    7-print_tebahpla

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <ctype.h>
 /**
- * main - Prints the alphabet in lowercase and then in uppercase.
+ * main - Prints the alphabet in lowercase.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-int letter;
-for (letter = 'z'; letter <= 'a'; letter++)
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
 putchar(letter);
 putchar('\n');
 return (0);
