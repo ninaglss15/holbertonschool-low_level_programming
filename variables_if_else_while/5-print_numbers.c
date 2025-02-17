22672cfe96494bd542cb6560f9661d9b497350a5
ninaglss15 <10526@holbertonstudents.com>
2025-02-17 09:38:53 +0000

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int numbers;
+for (numbers = '0'; numbers <= '10'; numbers++)
+putchar(letter);
+putchar('\n');
+return (0);
+}
