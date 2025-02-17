12d6238d7d66d7057930f5e9c41e11df2b01dbbb
ninaglss15 <10526@holbertonstudents.com>
2025-02-17 10:10:06 +0000

    6-print_numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int numbers;
+for (numbers = '0'; numbers <= '9'; numbers++)
+putchar(numbers);
+putchar('\n');
+return (0);
+}
