a3f4c3bdf8cbc6d80bb872318eded59a4b775594
ninaglss15 <10526@holbertonstudents.com>
2025-02-18 08:32:04 +0000

    0-putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Puts "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+printf("_putchar/n");
+return (0);
+}
