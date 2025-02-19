f59d781a28b37693d3692e3b9f2581c89095aba7
ninaglss15 <10526@holbertonstudents.com>
2025-02-19 08:51:01 +0000

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 'a'; num <= 'z'; num++)
+	putchar('\n');
+	putchar(num);
+	return (0);
+}
