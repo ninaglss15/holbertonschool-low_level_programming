0283f7c376fa9e66927bddedc724531ffb738d3f
ninaglss15 <10526@holbertonstudents.com>
2025-02-19 13:58:30 +0000

    2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+#include <stdio.h>
 /**
  * print_alphabet_x10 - Prints the alphabet in lowercase and then in uppercase.
  *
@@ -9,10 +10,23 @@ void print_alphabet_x10(void)
 {
 	int num;
 	int c;
-{
-	for (c = 0; c < 10; c++)
+
+	for (c = 0; c < 10; c)
+	{
+		for (num = 'a'; num <= 'z'; num++)
+		{
+			putchar(num);
+			putchar('\n');
+		}
+	}
 }
-	for (num = 'a'; num <= 'z'; num++)
-	_putchar(num);
-	_putchar('\n');
+/**
+ * main - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
 }
