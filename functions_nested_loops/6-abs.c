35ff8f0f49546b08a560c5b6467cd8128d56c86b
ninaglss15 <10526@holbertonstudents.com>
2025-02-21 09:44:03 +0000

    6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - Prints the alphabet in lowercase and then in uppercase.
+ *
+ *@tomcruz: dfhdkjhckeuyg
+ *
+ * Return: 0
+ */
+
+int _abs(int tomcruz)
+{
+
+	if (tomcruz < 0)
+	return (-tomcruz);
+	else
+	return (tomcruz);
+}
