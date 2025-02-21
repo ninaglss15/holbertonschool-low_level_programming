9c78ade33309fda789ee74af6efb0f374e5a4b3f
ninaglss15 <10526@holbertonstudents.com>
2025-02-21 21:00:07 +0100

    10-main.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - prints the 9 times table
+ *
+ *@num1:sdyhvbsjvh
+ *
+ *@num2:sjhduilxwkbcj
+ *
+ * Return: returns nothing
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
