73c79d04e44fcd4026c3a2376e0f8e9ea62283ba
ninaglss15 <10526@holbertonstudents.com>
2025-02-21 19:25:50 +0100

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,12 +16,12 @@ void jack_bauer(void)
 	for (min = 0; min < 60; min++)
 	{
 
-			_putchar((hour / 10) + '0');
-			_putchar((hour % 10) + '0');
-			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min % 10) + '0');
-			_putchar(10);
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar(10);
 		}
 	}
 }
