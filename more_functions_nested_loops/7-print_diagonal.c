abf84a71d3f1b16169ae75e6d565f548f6cb88d7
ninaglss15 <10526@holbertonstudents.com>
2025-02-24 22:05:44 +0100

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_line - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ *
+ *@n: frjfnuhuyguigujkns
+ */
+void print_diagonal(int n)
+{
+	int y;
+	int x;
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (y = 0; y < n; y++)
+	{
+	for (x = 0; x < y; x++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+}
+
+
