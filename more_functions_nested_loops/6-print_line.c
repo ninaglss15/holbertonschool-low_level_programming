a69b34c645a6c4c945181976fb4646611955fb7d
ninaglss15 <10526@holbertonstudents.com>
2025-02-24 14:09:43 +0100

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_line - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ *
+ *@n: frjfnujkns
+ */
+void print_line(int n)
+{
+	if (n > 0)
+	write(1, "__________", 10);
+	_putchar('\n');
+}
