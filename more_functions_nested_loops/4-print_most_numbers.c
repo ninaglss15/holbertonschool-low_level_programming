d62d8912fc58dbdc48bc713fff3c59e227fcee17
ninaglss15 <10526@holbertonstudents.com>
2025-02-24 10:45:11 +0100

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_most_numbers - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+int num;
+
+for (num = '0'; num <= '9'; num++)
+if (num != '2' && num != '4')
+putchar(num);
+putchar('\n');
+}
