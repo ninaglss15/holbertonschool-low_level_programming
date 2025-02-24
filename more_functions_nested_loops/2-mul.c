509ee94433bc5189567454ca5aedee0a5c2751c2
ninaglss15 <10526@holbertonstudents.com>
2025-02-24 10:13:03 +0100

    2-mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * mul - Prints the alphabet in lowercase and then in uppercase.
+ *
+ * @b:dieued
+ *
+ * @a: ijdoico
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
