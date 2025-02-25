4da307d6b64d3d4636ef3fa9c4fe4265a3688c29
ninaglss15 <10526@holbertonstudents.com>
2025-02-25 12:06:53 +0100

    9-fizz_buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,40 +1,52 @@
-   #include "main.h"
+#include "main.h"
 #include <unistd.h>
 #include <stdio.h>
 /**
- * print_square - dlkdubckjdbnjfb
+ * main - dlkdubckjdbnjfb
  *
  * Return: Always 0.
  *
- *@size: frjfnuhuyguigujkns
  */
 int main(void)
 {
 
-int n;
+	int n;
 
-for (n = 1 ; n <= 100; n++)
+	for (n = 1 ; n <= 100; n++)
+	{
 
-if ((n % 3 == 0) && (n % 5 == 0))
-{
-printf("FizzBuzz");
-}
-else if (n % 3 == 0)
-{
-printf("fizz");
-}
-else if (n % 5 	== 0)
-{
-printf("buzz");
-}
-else
-{
-printf("%d", n);
-}
-if (n != 100)
-{
-printf(" ");
-}
-printf("\n");
-return(0);
+	if ((n % 3 == 0) && (n % 5 == 0))
+	{
+
+	printf("FizzBuzz");
+	}
+
+	else if (n % 3 == 0)
+	{
+
+	printf("fizz");
+	}
+
+	else if (n % 5 == 0)
+	{
+
+	printf("buzz");
+	}
+
+	else
+	{
+
+	printf("%d", n);
+	}
+
+	if (n != 100)
+	{
+
+	printf(" ");
+	}
+
+	}
+
+	printf("\n");
+	return (0);
 }
