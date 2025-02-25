8c5feb53e983a42c4215abc10af9466df6c45831
ninaglss15 <10526@holbertonstudents.com>
2025-02-25 09:58:10 +0100

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * print_square - dlkdubckjdbnjfb
+ *
+ * Return: Always 0.
+ *
+ *@size: frjfnuhuyguigujkns
+ */
+void print_square(int size)
+{
+	int y;
+	int x;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (y = 0; y < size; y++)
+	{
+	for (x = 0; x < size; x++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
