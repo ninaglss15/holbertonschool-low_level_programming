12af1e61ef9982bd9ca44952209b949329ff53be
ninaglss15 <10526@holbertonstudents.com>
2025-02-27 15:35:32 +0100

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - check the code
+ *
+ *@a: dfdxcesfsqfqre
+ *
+ *@b: oijdoiehpiu
+ *
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
