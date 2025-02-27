1ca6f1a11933ae3e50f86cdc4386e7847a2d8221
ninaglss15 <10526@holbertonstudents.com>
2025-02-27 18:29:38 +0100

    2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - check the code
+ *
+ *@s: dfdxcesfsqfqre
+ *
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	return(length);
+}
