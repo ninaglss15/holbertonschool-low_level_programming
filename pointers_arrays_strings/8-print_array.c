6ab4098a2be636c62b0386b47e2f35062bdc9a11
ninaglss15 <10526@holbertonstudents.com>
2025-03-01 13:49:13 +0100

    8-print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_array - check the code
+ *
+ *@a: dfdxcesfsqfqre
+ *
+ *@n: kdfknrjfk
+ *
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	if (i == n - 1)
+	printf("%d", a[i]);
+
+	else
+
+	printf("%d, ", a[i]);
+}
