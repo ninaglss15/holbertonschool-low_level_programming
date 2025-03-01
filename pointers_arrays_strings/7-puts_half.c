53341573ee56fd20f34a4d660b5dbf9d88e3b2df
ninaglss15 <10526@holbertonstudents.com>
2025-03-01 13:24:38 +0100

    7-puts_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -11,12 +11,19 @@
  */
 void puts_half(char *str)
 {
-	int i;
+	int a, n, l;
 
-	for (i = 5; str[i] != '\0'; i++)
-	{
-	_putchar(str[i]);
-	}
+	l = 0;
 
+	for (a = 0; str[a] != '\0'; a++)
+		l++;
+
+	n = (l / 2);
+
+	if ((l % 2) == 1)
+		n = ((l + 1) / 2);
+
+	for (a = n; str[a] != '\0'; a++)
+		_putchar(str[a]);
 	_putchar('\n');
 }
