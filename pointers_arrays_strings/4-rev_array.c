2d18f03d30ecbd23f8cd3b5140bcc94f9165a000
ninaglss15 <10526@holbertonstudents.com>
2025-03-02 16:35:44 +0100

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - check the code
+ *
+ *@a: dfdxcesfsqfqre
+ *
+ *@n: rjrbcj
+ *
+ * Return: Always 0.
+ */
+void reverse_array(int *a, int n)
+{
+	int length = 0;
+
+
+	for (; s[length] != '\0'; length++)
+	{
+	}
+	for (; length > 0; length--)
+	{
+	_putchar(s[length - 1]);
+	}
+
+	_putchar('\n');
+}
+
