53355b2e0fca147675de7e4359e5dfc40d2e20e3
ninaglss15 <10526@holbertonstudents.com>
2025-03-03 15:00:05 +0100

    2-strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strchr - check the code
+ *
+ *@s: dfdxcesfsqfqre
+ *
+ *@c: hcgyucdh
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+
+	return (NULL);
+
+}
