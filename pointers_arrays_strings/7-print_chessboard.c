1cd1d9fa484334e57e5840d6506146154fd9cf59
ninaglss15 <10526@holbertonstudents.com>
2025-03-03 19:23:52 +0100

    7-print_chessboa

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_chessboard - check the code
+ *
+ *@a: dfdxcesfsqfqre
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c;
+	int l;
+
+	for (c = 0; c < 8; c++)
+	for (l = 0; l < 8; l++)
+
+	_putchar(a[c][l]);
+
+	_putchar('\n');
+}
