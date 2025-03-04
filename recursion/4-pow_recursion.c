31cd39f0a38f9a50f6f206d3a1c33775b73dfd33
ninaglss15 <10526@holbertonstudents.com>
2025-03-04 14:13:33 +0100

    4-pow.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - check the code
+ *
+ *@s: dfdxcesfsqfqre
+ *
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	return (-1);
+
+	else if (y == 0)
+	return (1);
+
+	else
+	return (x * _pow_recursion( x, y - 1));
+}
