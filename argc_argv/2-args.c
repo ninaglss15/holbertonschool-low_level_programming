78782d85e562b5553f64bb40ea9ca697b3a02d86
ninaglss15 <10526@holbertonstudents.com>
2025-03-05 18:02:24 +0100

    2-args.c

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ *@argc: dfdxcesfsqfqre
+ *
+ *@argv: iejciu
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	printf("%s\n", argv[i]);
+	return (0);
+}
