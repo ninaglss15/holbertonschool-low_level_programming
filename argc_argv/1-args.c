b80e2950f04dd8133eefa82a9e7f3abd3365438a
ninaglss15 <10526@holbertonstudents.com>
2025-03-05 17:55:49 +0100

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ *@argc: dfdxcesfsqfqre
+ *
+ *@argv: iejciu
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
