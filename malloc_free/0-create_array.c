9285b5adfffc1b33e9f581421468d3955df130ce
ninaglss15 <10526@holbertonstudents.com>
2025-03-10 10:37:56 +0100

    0-create_array.c

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,22 +1,22 @@
 #include <stdlib.h>
 /**
- * *_strcpy - check the code
+ * *create_array - check the code
  *
- *@src: dfdxcesfsqfqre
+ *@c: dfdxcesfsqfqre
  *
- *@dest: lkfkrfk
+ *@size: lkfkrfk
  *
  * Return: Always 0.
  */
 char *create_array(unsigned int size, char c)
 {
 	char *m = NULL;
-	int unsigned i;
+	unsigned int i;
 
 	if (size == 0)
 	return (NULL);
 
-	m = (char *)malloc(size*sizeof(char));
+	m = (char *)malloc(size * sizeof(char));
 
 	if (m == NULL)
 	return (NULL);
