574d89f98e49ab4632322f270d21aeae08df1ee4
ninaglss15 <10526@holbertonstudents.com>
2025-03-10 10:59:42 +0100

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+/**
+ * *_strdup - check the code
+ *
+ *@str: lkfkrfk
+ *
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	char *p = NULL;
+	int i;
+
+	if (str == 0)
+	return (NULL);
+
+	p = (char *)malloc((strlen(str) + 1) * sizeof(char));
+
+	if (str == 0)
+	return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	p[i] = str[i];
+
+	return (p);
+}
