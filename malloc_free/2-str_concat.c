5ec7a6cc704f3226b3fd6c88474805ba2f6e3c29
ninaglss15 <10526@holbertonstudents.com>
2025-03-10 13:13:47 +0100

    2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,37 +16,36 @@ char *str_concat(char *s1, char *s2)
 	int c2 = 0;
 	char *p;
 
-	 if (s1 == 0)
-        s1 = "";
+	if (s1 == 0)
+	s1 = "";
+	if (s2 == 0)
+	s2 = "";
 
-        if (s2 == 0)
-        s2 = "";
+	while (s1[c1])
+	c1++;
 
-        while (s1[c1])
-        c1++;
+	while (s2[c2])
+	c2++;
 
-        while (s2[c2])
-        c2++;
-
-        i = c1 + c2;
-        p = malloc((sizeof(char) * i) + 1);
-        if (p == NULL)
-        return (NULL);
+	i = c1 + c2;
+	p = malloc((sizeof(char) * i) + 1);
+	if (p == NULL)
+	return (NULL);
 
 
-        while (o < i)
-        {
+	while (o < i)
+	{
 
-        if (o <= c1)
-        p[o] = s1[o];
+	if (o <= c1)
+	p[o] = s1[o];
 
-        if (o >= i)
-        {
-        p[o] = s2[c2];
-        c2++;
-        }
-        o++;
-        }
-        p[o] = '\0';
-        return (p);
+	if (o >= i)
+	{
+	p[o] = s2[c2];
+	c2++;
+	}
+	o++;
+	}
+	p[o] = '\0';
+	return (p);
 }
