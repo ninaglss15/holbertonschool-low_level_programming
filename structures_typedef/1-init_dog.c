7e1d7067438acf625ee553fc3fc558007ad7f65d
ninaglss15 <10526@holbertonstudents.com>
2025-03-12 15:18:25 +0100

    update

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-
+#include <stdlib.h>
 #include "dog.h"
 #include <string.h>
 #include <string.h>
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,7 +7,7 @@ typedef struct dog {
     char *owner;
 } dog;
 
-void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
 
