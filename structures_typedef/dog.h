f2f982645096b6c84407c233bbda34a92dbfafe9
ninaglss15 <10526@holbertonstudents.com>
2025-03-13 14:28:34 +0100

    update

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,16 +6,16 @@
 * @name: Pointer to a string for dog's name
 * @age: Float value for dog's age
 * @owner: Pointer to a string for owner's name
-*
 * Description: Defines a new struct to store information about a dog,
 * including its name, age, and owner.
 */
 
-struct dog {
+struct dog
+{
     
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
