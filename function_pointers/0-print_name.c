1e0d895146ad52d6b9276f16caacf94b44465e6c
ninaglss15 <10526@holbertonstudents.com>
2025-03-17 12:35:45 +0100

    ece

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,25 +1,16 @@
 #include "function_pointers.h"
+#include <stdio.h>
+
 /**
-<<<<<<< HEAD
-* print_name - Prints a name using the function passed as a pointer
-* @name: The name of the person
-* @f: A pointer to a function that takes a char pointer as argument
-*
-* Return: Nothing
-*/
-	void print_name(char *name, void (*f)(char *))
-{
-	if (name && f)
-=======
- * print_name - Prints a name
- * @name: The name to prints
- * @f: Pointer to function
+ * print_name - Prints a name using the function passed as a pointer
+ * @name: The name of the person
+ * @f: A pointer to a function that takes a char pointer as argument
  *
  * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
->>>>>>> e1a8d2b0a8ea742d08c863dc6f62170f09baea37
-		f(name);
+	if (name && f)
+	f(name);
 }
+
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 #ifndef _HEADER_
 #define _HEADER_
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 
-
-=======
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
-#include <stdlib.h>
-
-
-void print_name(char *name, void (*f)(char *));
-
->>>>>>> e1a8d2b0a8ea742d08c863dc6f62170f09baea37
 #endif
