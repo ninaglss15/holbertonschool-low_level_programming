1a53acd44d76d17855a09d52eade1cd46101e075
ninaglss15 <10526@holbertonstudents.com>
2025-03-17 11:14:33 +0100

    nouveau commit

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+* print_name - Prints a name using the function passed as a pointer
+* @name: The name of the person
+* @f: A pointer to a function that takes a char pointer as argument
+*
+* Return: Nothing
+*/
+	void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
