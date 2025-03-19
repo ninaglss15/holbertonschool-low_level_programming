c2fe87dba3f7ffcb4bd9586cf4cf65ff1391ae3f
ninaglss15 <10526@holbertonstudents.com>
2025-03-19 11:48:17 +0100

    nouveau commit n°4 probleme exo 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -23,11 +23,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	return;
 
 	for (i = 0; i < n; i++)
-    {
+	{
 	printf("%d", va_arg(args, int));
 	if (separator != NULL && i < n - 1)
 	printf("%s", separator);
-    }
+	}
 
 	va_end(args);
 
