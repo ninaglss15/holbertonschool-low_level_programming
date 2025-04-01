a1e30b0de703b940bb0ca037d72b680ba995a235
ninaglss15 <10526@holbertonstudents.com>
2025-04-01 12:05:08 +0200

    exo deux peush

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* add_node - Ajoute un nouveau nœud au début de la liste
+* @head: Pointeur vers le premier nœud de la liste
+* @str: La chaîne de caractères à ajouter au nœud
+* Return: L'adresse du nouveau nœud, ou NULL en cas d'échec
+*/
+
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int length = 0;
+
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL || head == NULL)
+	{
+	return (NULL);
+	free(new);
+	}
+    while (str[length] != '\0')
+    {
+	length++;
+    }
+
+	new->str = strdup(str);
+	new->len = length;
+	new->next = *head;
+
+	*head = new;
+	return (new);
+}
+
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
 * struct list_s - structure d'une liste simplement chaînée
@@ -14,12 +15,15 @@
 typedef struct list_s
 {
 char *str;
-int len;
+
+unsigned int len;
+
 struct list_s *next;
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 
 #endif
 
