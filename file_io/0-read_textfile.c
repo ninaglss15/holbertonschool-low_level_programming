cede920d185dc37bbd7a08e67a17ccca71ae8c6e
ninaglss15 <10526@holbertonstudents.com>
2025-04-06 16:59:23 +0200

    push first exercice

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -21,8 +21,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	ssize_t nrd, nwr;
 
-	if (*filename == '\0')
+	if (filename == NULL)
 	return (0);
+
 
 	fd = open(filename, O_RDONLY);
 
